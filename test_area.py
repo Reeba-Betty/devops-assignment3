2878547500d8629c25dbd4cd016ea375821fd343
Reeba Mary Betty <reebadarson17@gmail.com>
2024-03-22 21:00:16 -0400

    Update test_area.py

--- test_area.py
+++ test_area.py
@@ -1,12 +1,13 @@
 import pytest  
 from src.area import calculate_area_square  
-#function for test calculate
+  
 def test_calculate_area_square():  
     assert calculate_area_square(2) == 4  
     assert calculate_area_square(2.5) == 6.25
     #last 4 digits of student id 
     #add last 4 
-    assert calculate_area_square(3) == 7568
+    assert calculate_area_square(3) == 9
+    assert calculate_area_square(31) == 961
   
 def test_calculate_area_square_negative():  
     with pytest.raises(TypeError):  
